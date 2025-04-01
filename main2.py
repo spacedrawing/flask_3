7d3c0fccbca47e55aad9386ae2e718ee52350e18
Space Drawing <kulevojdanil@gmail.com>
2025-04-01 17:57:58 +0300

    add /training

--- main2.py
+++ main2.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def inndex():
     return render_template('base.html', username='Володя', title='Ок')
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof, title=f'{prof}ам, не рады')
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='8080')
