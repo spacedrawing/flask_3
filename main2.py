55d08d2967e1f7bff14ab561ff49ac147fd082d0
Space Drawing <kulevojdanil@gmail.com>
2025-04-01 19:13:20 +0300

    add /list_prof

--- main2.py
+++ main2.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
@@ -15,7 +16,17 @@ def training(prof):
 
 @app.route('/list_prof/<list>')
 def list_prof(list):
-    return render_template('list_prof.html', prof_list=prof_list, list_type=list)
+    return render_template('list_prof.html', prof_list=prof_list, list_type=list.lower())
+
+
+@app.route('/news')
+def news():
+    with open("news.json", "rt", encoding="utf8") as f:
+        news_list = json.loads(f.read())
+    print(news_list)
+    return render_template('news.html', news=news_list)
+
+    
 
 
 if __name__ == '__main__':
